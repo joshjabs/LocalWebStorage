d48567ba0e582c81b2600a68dbb370a981011892
Joshua <JoshuaAbshear@gmail.com>
2021-03-12 00:05:01 -0700

    pseudo code for lab

--- storage.js
+++ storage.js
@@ -1,62 +1,59 @@
 // Manage Local Storage
 
 function addToLocalStorage() {
-    var userInput = document.getElementById("userInput").value
+    
+    // PSEUDO CODE
 
-    if (userInput != "") {
+    // First, create a variable userInput with HTML Element's userInput value
 
-        console.log("adding " + userInput + " to Local Storage")
+    // IF userinput is not an empty string
+        // log the variable value
+        // get current local storage or empty string if not defined with localStorage.getItem(localStorageKey123)
 
-        // get current local storage or empty string if not defined
-        var cLocalStorage = localStorage.getItem("localStorageKey123") || ""
+        // create new variable with previous local storage and append userInput and newline
+        // update the local storage with our new string value using localStorage.setItem(localStorageKey123, newValue)
+        // updateChatArea()
 
-        // reset local storage object with appended userInput and newline
-        var newLocalStorage = cLocalStorage + userInput + "\n"
-        localStorage.setItem("localStorageKey123", newLocalStorage)
-
-        updateChatArea()
-
-    } else {
-        console.warn("no user input entered")
-        alert("No User Input to add to localStorage")
-    }
-
+    // ELSE 
+        // console.warn("no user input entered")
+        // alert("No User Input to add to localStorage")
+    
 }
 
 function clearLocalStorage() {
-    console.log("clearing local storage...")
-    localStorage.clear()
-    updateChatArea()
+    // console.log("clearing local storage...")
+    // localStorage.clear()
+    // updateChatArea()
 }
 
 // Manage Session Storage
 
-function addToSessionStorage() {
-    var userInput = document.getElementById("userInput").value
+// function addToSessionStorage() {
+//     var userInput = document.getElementById("userInput").value
 
-    if (userInput != "") {
-        console.log("adding " + userInput + " to Session Storage")
+//     if (userInput != "") {
+//         console.log("adding " + userInput + " to Session Storage")
 
-        // get current session storage or empty string if not defined
-        var cSessionStorage = sessionStorage.getItem("sessionStorageKey123") || ""
+//         // get current session storage or empty string if not defined
+//         var cSessionStorage = sessionStorage.getItem("sessionStorageKey123") || ""
 
-        // reset session storage object with appended userInput and newline
-        var newSessionStorage = cSessionStorage + userInput + "\n"
-        sessionStorage.setItem("sessionStorageKey123", newSessionStorage)
+//         // reset session storage object with appended userInput and newline
+//         var newSessionStorage = cSessionStorage + userInput + "\n"
+//         sessionStorage.setItem("sessionStorageKey123", newSessionStorage)
 
-        updateChatArea()
-    } else {
-        console.warn("no user input entered")
-        alert("No User Input to add to sessionStorage")
-    }
+//         updateChatArea()
+//     } else {
+//         console.warn("no user input entered")
+//         alert("No User Input to add to sessionStorage")
+//     }
 
-}
+// }
 
-function clearSessionStorage() {
-    console.log("clearing session storage...")
-    sessionStorage.clear()
-    updateChatArea()
-}
+// function clearSessionStorage() {
+//     console.log("clearing session storage...")
+//     sessionStorage.clear()
+//     updateChatArea()
+// }
 
 
 // Common
